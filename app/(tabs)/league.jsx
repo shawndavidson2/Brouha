4efa37937c7c586a60ca9f5b3ba87969a884ef2d
shawndavidson2 <davidson.791@osu.edu>
2024-06-28 14:45:45 -0400

    Added profile button in top right of leagues page

--- app/(tabs)/league.jsx
+++ app/(tabs)/league.jsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from 'expo-router'
+import Profile from '../profile'
+import { FlatList } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import { icons } from '../../constants'
+import { Image } from 'react-native'
 
 const League = () => {
+    const profile = async () => {
+        router.push("../profile")
+    };
+
     return (
-        <View>
-            <Text>League</Text>
-        </View>
+        <SafeAreaView className="bg-green-100 h-full">
+            <FlatList
+                ListHeaderComponent={() => (
+                    <View className="w-full flex justify-center items-center mt-6 mb-12 px-8">
+                        <TouchableOpacity
+                            onPress={profile}
+                            className="flex w-full items-end mb-10"
+                        >
+                            <Image
+                                source={icons.profile}
+                                resizeMode="contain"
+                                className="w-8 h-8"
+                            />
+                        </TouchableOpacity>
+
+                    </View>
+                )}
+            />
+        </SafeAreaView>
     )
 }
 
