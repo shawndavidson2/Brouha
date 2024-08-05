fb69c672322b53c41324caef06c7a45e04f2da20
shawndavidson2 <davidson.791@osu.edu>
2024-08-04 20:36:09 -0400

    . appwrite stuff

--- app/(tabs)/pick-lineup.jsx
+++ app/(tabs)/pick-lineup.jsx
@@ -55,7 +55,7 @@ const PickLineup = () => {
                             <Swipeable
                                 key={pick.$id}
                                 renderRightActions={(progress, dragX) =>
-                                    renderRightActions(pick, progress, dragX, () => deletePickFromPL(pick, pick.$id))
+                                    renderRightActions(pick, progress, dragX, () => deletePickFromPL(pick.$id))
                                 }
                             >
                                 <View style={styles.pickItem}>
