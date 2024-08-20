65744e2b2168a62d91b87c72c93f58b9eec6e2fa
shawndavidson2 <davidson.791@osu.edu>
2024-08-20 12:48:38 -0400

    .

--- excel_upload/src/main.js
+++ excel_upload/src/main.js
@@ -9,19 +9,19 @@ export default async ({ req, res, log, error }) => {
   //    .setEndpoint('https://cloud.appwrite.io/v1')
   //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
   //    .setKey(process.env.APPWRITE_API_KEY);
+  log(req)
+  // // You can log messages to the console
+  // log('Hello, Logs!');
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  // // If something goes wrong, log an error
+  // error('Hello, Errors!');
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+  // // The `req` object contains the request data
+  // if (req.method === 'GET') {
+  //   // Send a response with the res object helpers
+  //   // `res.send()` dispatches a string back to the client
+  //   return res.send('Hello, World!');
+  // }
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
