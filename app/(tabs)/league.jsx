562d58aaa946c12fdc376d896b1a5e6b5de52cf5
shawndavidson2 <davidson.791@osu.edu>
2024-09-04 22:19:37 -0400

    Got rid of unused code (UpdateUserStats)

--- app/(tabs)/league.jsx
+++ app/(tabs)/league.jsx
@@ -9,7 +9,6 @@ import LeagueStats from '../../components/league/LeagueStats';
 import LeagueTitleAndProfile from '../../components/league/LeagueTitleAndProfile';
 import JoinLeagueButton from '../../components/league/JoinLeagueButton';
 import { useGlobalContext } from '../../context/GlobalProvider';
-import { UpdateUserStats } from '../../components/UpdateUserStats';
 import { useLineupCache } from '../../context/lineupContext';
 import { useRefresh } from '../../context/RefreshContext';
 import Loading from '../../components/Loading';
@@ -21,19 +20,6 @@ const League = () => {
     const { triggerRefresh } = useRefresh();
     const lineupCache = useLineupCache();
     const [refreshKey, setRefreshKey] = useState(0); // Add a refresh key state
-
-    // When the user first logs in, we want to update their stats...
-    useEffect(() => {
-
-        const updateStats = async () => {
-            if (isGlobalInitialized && league) {
-                setLoading(true);  // Set loading to true before updating stats
-                //await UpdateUserStats(user, setUser, league, setLeague, weekNum, lineupCache);
-                setLoading(false); // Set loading to false after updating stats
-            }
-        };
-        updateStats();
-    }, [isGlobalInitialized]);
 
     const joinLeague = () => {
         router.push("../join-league");
