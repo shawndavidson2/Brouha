4dbb2a2478ce6eb82c211b1385bc5917b0dc28a9
shawndavidson2 <davidson.791@osu.edu>
2024-09-09 22:19:10 -0400

    added a delay between updating picks from excel

--- excel_upload/src/main.js
+++ excel_upload/src/main.js
@@ -4,7 +4,7 @@ import { getPicksByWeek, retryUpdatePickStatus, getWeekNum } from './db.js';
 
 
 //const req = { "bodyRaw": "{\"$id\":\"66c8dea00fb779e93858\",\"bucketId\":\"667edd29003dd0cf6445\",\"$createdAt\":\"2024-08-20T16:49:14.433+00:00\",\"$updatedAt\":\"2024-08-20T16:49:14.433+00:00\",\"$permissions\":[],\"name\":\"Matchup Data WK1.xlsx\",\"signature\":\"04b7352b4c23334bdf0eebb9feff4b51\",\"mimeType\":\"application\\/vnd.openxmlformats-officedocument.spreadsheetml.sheet\",\"sizeOriginal\":25522,\"chunksTotal\":1,\"chunksUploaded\":1}", "body": { "$id": "66c8dea00fb779e93858", "bucketId": "667edd29003dd0cf6445", "$createdAt": "2024-08-20T16:49:14.433+00:00", "$updatedAt": "2024-08-20T16:49:14.433+00:00", "$permissions": [], "name": "Matchup Data WK1.xlsx", "signature": "04b7352b4c23334bdf0eebb9feff4b51", "mimeType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "sizeOriginal": 25522, "chunksTotal": 1, "chunksUploaded": 1 }, "headers": { "host": "66c4c90a8c89d:3000", "user-agent": "Appwrite/1.5.10", "content-type": "application/json", "x-appwrite-trigger": "event", "x-appwrite-event": "buckets.667edd29003dd0cf6445.files.66c4bb05d31fc5e2d2a8.create", "connection": "keep-alive", "content-length": "386" }, "method": "POST", "host": "66c4c90a8c89d", "scheme": "http", "query": {}, "queryString": "", "port": 3000, "url": "http://66c4c90a8c89d:3000/", "path": "/" }
-
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 // This is your Appwrite function
 // It's executed each time we get a request
@@ -59,6 +59,8 @@ export default async ({ req, res, log, error }) => {
                 if (matchedPick && jsonPickStatus !== matchedPick["status"]) {
                   //matchedPick["status"] = jsonPick[sheetName];
                   log("Changed pick: " + matchedPick["pick-title"] + " from " + matchedPick["status"] + " to " + jsonPick[sheetName])
+                  // Add a 5-second delay before calling retryUpdatePickStatus
+                  await delay(5000);
                   await retryUpdatePickStatus(jsonPick[sheetName], matchedPick.$id);
                 }
               }
