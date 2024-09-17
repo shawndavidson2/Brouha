a3e3ef3639fb239c9933484db85f1bf3bb97b2b1
shawndavidson2 <davidson.791@osu.edu>
2024-09-16 23:54:38 -0400

    correction <-

--- reset_week_points/src/main.js
+++ reset_week_points/src/main.js
@@ -2,7 +2,7 @@ import { resetWeek, checkAndUpdateWeekNum } from './db.js'
 
 const calculateCurrentWeekNum = async () => {
   // Hardcoded start date for Week 1 (September 3, 2024 at 8 PM EDT)
-  const startWeek1 = new Date('2024-09-0T20:00:00-04:00');  // 8 PM EDT
+  const startWeek1 = new Date('2024-09-03T20:00:00-04:00');  // 8 PM EDT
   const currentDate = new Date();
   const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
   const weeksSinceStart = Math.floor((currentDate - startWeek1) / millisecondsPerWeek);
